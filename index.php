db97e4f10c6c39f59f880b9faff847232fa4f5d7
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-12-07 08:23:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,28 +6,28 @@
 </head>
 <p>Please input the Opposite side and Hypotenuse</p>
 <label for="x">Opposite Side:</label>
-<input type="number" id="x" name="x"> 
+<input type="number" id="x" name="x">
+<br>
 <label for="y">hypotenuse:</label>
-<input type="number" id="y" name="y"> 
+<input type="number" id="y" name="y">
+<br>
 <button onclick="return calc();">Submit</button>
 <p id="answer"></p>
     <script>
         function calc() {
             console.log("hi");
-            let xval1 = document.getElementById('x1').value;
-            let yval1 = document.getElementById('y1').value;
-            let xval2 = document.getElementById('x2').value;
-            let yval2 = document.getElementById('y2').value;
+            let xval = document.getElementById('x').value;
+            let yval = document.getElementById('y').value;
             var xhttp = new XMLHttpRequest();
             xhttp.onreadstatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
+                if (this.readyState == 2 && this.status == 200) {
                     console.log(document.getElementById('answer'));
                     document.getElementById('answer').innerHTML = "Your angle in degrees is : " + this.responseText;
                 }
             };
             xhttp.open("POST", 'https://us-east1-capable-arbor-286903.cloudfunctions.net/function-final', true);
             xhttp.setRequestHeader("Content-type", "application/json");
-            xhttp.send(JSON.stringify({x1:xval1, y1:yval1, x2:xval2, y2:yval2}));
+            xhttp.send(JSON.stringify({x1:xval, y1:yval}));
             console.log("bye");
         }
     </script>
