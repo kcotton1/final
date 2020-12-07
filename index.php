e2093cd46f3399eaae9e59c42908fe462f29a511
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-12-07 08:30:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
 <meta charset="utf-8">
 </head>
 <p>Please input the Opposite side and Hypotenuse</p>
+<div class="finals">
 <label for="x">Opposite Side:</label>
 <input type="number" id="x" name="x">
-<br>
-<p />
-<br>
-<label for="y">hypotenuse:</label>
+<label for="y">Hypotenuse:</label>
 <input type="number" id="y" name="y">
-<br>
 <button onclick="return calc();">Submit</button>
 <p id="answer"></p>
+</div>
     <script>
         function calc() {
             console.log("hi");
@@ -27,9 +25,9 @@
                     document.getElementById('answer').innerHTML = "Your angle in degrees is : " + this.responseText;
                 }
             };
-            xhttp.open("POST", 'https://us-east1-capable-arbor-286903.cloudfunctions.net/function-final', true);
+            xhttp.open("POST", 'https://us-east1-kcotton.cloudfunctions.net/function-sine', true);
             xhttp.setRequestHeader("Content-type", "application/json");
-            xhttp.send(JSON.stringify({x1:xval, y1:yval}));
+            xhttp.send(JSON.stringify({x:xval, y:yval}));
             console.log("bye");
         }
     </script>
